f931537a887cf112591bac2c25887fd484512c07
DanWhiteside <dpbwhiteside@gmail.com>
2024-09-05 16:08:21 +0100

    Update Mouse Amino acid comp.py

--- Unused Code/Mouse Amino acid comp.py
+++ Unused Code/Mouse Amino acid comp.py
@@ -16,15 +16,10 @@ import seaborn as sns
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 #%%
-
-# Define the column names as per GTF format
+#Collecting potential TSS from RNA seq data
 column_names = ["chrom", "source", "feature", "Left TS", "Right TS", "score", "strand", "frame", "attribute"]
-
-# Load the GTF file into a DataFrame
 gtf_file = 'mm9.knownGene.gtf'
 df = pd.read_csv(gtf_file, sep='\t', comment='#', names=column_names)
-
-# Filter for transcript features
 transcripts_df = df[df['feature'] == 'transcript']
 
 
@@ -58,10 +53,8 @@ mm9_strand = df_mouseChr1["strand"]
 Nucleosome Positions 
 '''
 
-# Define column names
 column_names = ['chromosome', 'center_position']
 
-# Load the text file into a DataFrame
 df_mouseN = pd.read_csv('unique.map_95pc.txt', delim_whitespace=True, header=None, names=column_names)
 
 #%%
@@ -69,15 +62,6 @@ df_mouseN = pd.read_csv('unique.map_95pc.txt', delim_whitespace=True, header=Non
 Mouse Genome
 '''
 def load_fasta_files_to_dict(directory):
-    """
-    Load all FASTA files from a directory into a dictionary containing only sequences.
-    
-    Parameters:
-    - directory: The path to the directory containing FASTA files.
-    
-    Returns:
-    - A dictionary where keys are filenames (without extensions) and values are sequences as strings.
-    """
     fasta_dict = {}
     for filename in os.listdir(directory):
         if filename.endswith(".fasta") or filename.endswith(".fa"):
@@ -115,9 +99,9 @@ num_to_chr = {'1': 'chr1', '2': 'chr2', '3': 'chr3', '4': 'chr4',
 
 print(chr_to_num["chr7"])
 
-print(df_mouseTS.columns)  # Check column names
-print(df_mouseTS.index)    # Check the index
-print(df_mouseTS.dtypes)  # Check data types of each column
+print(df_mouseTS.columns) 
+print(df_mouseTS.index)   
+print(df_mouseTS.dtypes)
 #%%
 '''
 Finding all nucleosomes within each gene 
@@ -167,9 +151,6 @@ def Nucleosome_N_Seq(Nuc_Number,df, genome, conversion_dict):
     Sequences = []
     Char_to_check = "N"
     for i in range(0,len(Position)):
-        # if i%50 == 0:
-        #     print(i)
-        #try:
         Current_nucleosomes = Position[i]
         #print(Current_nucleosomes)
         if Current_nucleosomes != "None":
@@ -191,8 +172,6 @@ def Nucleosome_N_Seq(Nuc_Number,df, genome, conversion_dict):
                 Sequences.append("None")
         else:
             Sequences.append("None")
-        #except:
-            #Sequences.append("None")
     return Sequences
 
 def average_Nucleosome(Nucleosomes):
@@ -253,7 +232,7 @@ def gene_finder(dataframe, chromosomes, conversion_dict):
 '''
 Codon table 
 '''
-# Define the codon to amino acid mapping
+
 codon_table = {
     'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L', 
     'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L', 
@@ -276,7 +255,6 @@ codon_table = {
 
 
 def extract_subsequence(full_gene, desired_sequence):
-    # Iterate over potential start positions of the desired sequence
     for i in range(len(desired_sequence)):
         adjusted_sequence = desired_sequence[i:]
         start = full_gene.find(adjusted_sequence)
@@ -302,17 +280,12 @@ def dna_to_amino(sequence, codon_table):
 
 
 def amino_acid_nucleosomes(full_gene, desired_sequence):
-    # Extract the correctly aligned subsequence
     try:
         subsequence, start_index, N_position = extract_subsequence(full_gene, desired_sequence)
-        # Adjust the start index to the nearest codon boundary
         adjusted_start_index = start_index % 3
-        # Extract the aligned DNA sequence for translation
         aligned_subsequence = full_gene[start_index - adjusted_start_index:start_index + len(desired_sequence)]
         protein_sequence = dna_to_amino(aligned_subsequence, codon_table)
-        # Determine how many amino acids to remove from the beginning
         amino_acids_to_remove = adjusted_start_index // 3
-        # Remove the unnecessary amino acids from the start
         final_protein_sequence = protein_sequence[amino_acids_to_remove:]
         return final_protein_sequence, start_index, N_position
     except:
@@ -338,51 +311,18 @@ def amino_acid_sequencer(dataframe):
         adjusted_start_seq.append(adj_n)
     return Amino_Acid_seq, adjusted_start_seq
 
-amino_acid_dict = {
-    "Alanine": "A",
-    "Arginine": "R",
-    "Asparagine": "N",
-    "Aspartic acid": "D",
-    "Cysteine": "C",
-    "Glutamic acid": "E",
-    "Glutamine": "Q",
-    "Glycine": "G",
-    "Histidine": "H",
-    "Isoleucine": "I",
-    "Leucine": "L",
-    "Lysine": "K",
-    "Methionine": "M",
-    "Phenylalanine": "F",
-    "Proline": "P",
-    "Serine": "S",
-    "Threonine": "T",
-    "Tryptophan": "W",
-    "Tyrosine": "Y",
-    "Valine": "V", 
+amino_acid_dict = {"Alanine": "A", "Arginine": "R", "Asparagine": "N", "Aspartic acid": "D",
+                   "Cysteine": "C", "Glutamic acid": "E", "Glutamine": "Q", "Glycine": "G",
+                   "Histidine": "H", "Isoleucine": "I", "Leucine": "L", "Lysine": "K",
+                   "Methionine": "M", "Phenylalanine": "F", "Proline": "P", "Serine": "S",
+                   "Threonine": "T", "Tryptophan": "W", "Tyrosine": "Y", "Valine": "V", 
 }
 
-amino_acids = [
-    "Alanine",
-    "Arginine",
-    "Asparagine",
-    "Aspartic acid",
-    "Cysteine",
-    "Glutamic acid",
-    "Glutamine",
-    "Glycine",
-    "Histidine",
-    "Isoleucine",
-    "Leucine",
-    "Lysine",
-    "Methionine",
-    "Phenylalanine",
-    "Proline",
-    "Serine",
-    "Threonine",
-    "Tryptophan",
-    "Tyrosine",
-    "Valine", 
-]
+amino_acids = ["Alanine", "Arginine", "Asparagine", "Aspartic acid",
+               "Cysteine", "Glutamic acid", "Glutamine", "Glycine",
+               "Histidine", "Isoleucine", "Leucine", "Lysine",
+               "Methionine", "Phenylalanine", "Proline", "Serine",
+               "Threonine", "Tryptophan", "Tyrosine", "Valine"]
 
 
 def amino_by_position(amino_acid, dataframe, amino_acid_dict):
@@ -392,21 +332,17 @@ def amino_by_position(amino_acid, dataframe, amino_acid_dict):
     pos = dataframe["N position on the gene"]
     pos = [item for item in pos if item != 'None']
     
-    # Offset the sequences that aren't aligned 
     padded_seqs = []
     for i in range(len(seq)):
         offset = int(pos[i]) // 3
         padded_seq = "x" * offset + seq[i]
         padded_seqs.append(padded_seq)
     
-    # Determine the maximum length of sequences for proper alignment
     max_length = len(max(padded_seqs, key=len))
-    
-    # Initialize counts for each position
+
     position_counts = [0] * max_length
     total_counts = [0] * max_length
     
-    # Count the occurrences of the target amino acid and valid sequences at each position
     for seq in padded_seqs:
         for i, aa in enumerate(seq):
             if i < max_length:
@@ -415,21 +351,13 @@ def amino_by_position(amino_acid, dataframe, amino_acid_dict):
                 if aa != 'x':  # Count only valid amino acids
                     total_counts[i] += 1
     
-    # Calculate abundance percentages
     abundance_percentages = [(position_counts[i] / total_counts[i]) * 100 if total_counts[i] > 0 else 0 for i in range(max_length)]
     abundance_percentages = abundance_percentages[:133]
     return abundance_percentages
 
-                
-    
-    # # Calculate abundance percentages
-    # position_counts = position_counts[:133]
-    # abundance_percentages = [(position_counts[i] / sum(position_counts)) * 100 for i in range(0,133)]
-    # return abundance_percentages
-
+            
 def count_all_amino(dataframe, amino_acid_dict, amino_list):
     amino_acid_abundances = {}
-    # Loop through each amino acid and calculate its abundance
     for amino in amino_list:
         abundance = amino_by_position(amino, dataframe, amino_acid_dict)  # Use the first letter of each amino acid
         amino_acid_abundances[amino] = abundance
@@ -437,13 +365,10 @@ def count_all_amino(dataframe, amino_acid_dict, amino_list):
 
 
 def sum_abundances(amino_acid_abundances):
-    # Find the length of sequences (assuming all have the same length)
     sequence_length = len(next(iter(amino_acid_abundances.values())))
     
-    # Initialize a list of zeros to store the sum of abundances at each position
     summed_abundances = [0] * sequence_length
     
-    # Loop through each amino acid's abundance
     for amino_acid, abundances in amino_acid_abundances.items():
         for i in range(sequence_length):
             summed_abundances[i] += abundances[i]
@@ -467,20 +392,18 @@ def count_all_by_nucleosome(nuc_num, dataframe, amino_acid_dict, amino_list, chr
 
 def heatmaps(nuc_num, amino_abundance):
     df = pd.DataFrame(amino_abundance)
-    
-    # Transpose the DataFrame to have positions as rows and amino acids as columns
+
     df = df.transpose()
     
-    # Plot the heatmap
+
     plt.figure(figsize=(14, 8))
     sns.heatmap(df, cmap="inferno", vmax=20, vmin=0 ,annot=False)
     
-    # Adding labels and title
+
     plt.xlabel("Position")
     plt.ylabel("Amino Acid")
     plt.title(f"Heatmap of Amino Acid Abundance Across Positions in the plus {nuc_num} Nucleosome of Cerevisiae (%)")
     
-    # Display the plot
     plt.tight_layout()
     plt.show()
 
@@ -502,47 +425,36 @@ def position_visualisation(amino_abundance):
         
 
 def pallet_plot(nuc_num, data_dict):
-    # Set the color palette
     palette = sns.color_palette("husl", len(data_dict))
     
-    # Determine the number of rows and columns for the grid
     num_plots = len(data_dict)
     cols = 4  # You can adjust this number to change the grid's shape
     rows = (num_plots + cols - 1) // cols  # Ceiling division to determine rows
     
-    # Create a smaller figure with subplots
     fig, axes = plt.subplots(rows, cols, figsize=(15, 9))
     
-    # Flatten the axes array for easy iteration
     axes = axes.flatten()
     
-    # Loop through the dictionary and plot each list in a separate subplot
     for idx, (key, value) in enumerate(data_dict.items()):
-        # Apply the function to the list
         transformed_data = position_visualisation(value)
         
-        # Plot the data on the corresponding subplot
         ax = axes[idx]
         ax.plot(transformed_data, color=palette[idx])
         ax.set_title(f"{key}")
         ax.set_xlabel("Position")
         ax.set_ylabel("Abundance (%)")
     
-    # Hide any unused subplots
     for i in range(idx + 1, len(axes)):
         fig.delaxes(axes[i])
     
-    # Add a super title for the entire grid
     fig.suptitle(f"Positional Abundance of Amino Acids in M. Musculus plus {nuc_num} Nucleosome", fontsize=14)
     
-    # Adjust layout
     plt.tight_layout()
     plt.subplots_adjust(top=0.9)
     plt.show()
 
 def sliding_window_heatmap(dictionary):
     for idx, (key, value) in enumerate(dictionary.items()):
-        # Apply the function to the list
         transformed_data = sliding_window_average(value)
         dictionary[key] = transformed_data
     return dictionary
@@ -565,16 +477,11 @@ mm9N9_aminos = count_all_by_nucleosome(9,df_mouseChr1, amino_acid_dict, amino_ac
 #%%
 mm9N5to9 = [mm9N5_aminos, mm9N6_aminos, mm9N7_aminos, mm9N8_aminos, mm9N9_aminos]
 
-# Initialize the result dictionary
 Average_amino = {}
 
-# Iterate over each key in the dictionaries
 for key in mm9N5_aminos.keys():
-    # Use zip to aggregate the values across all dictionaries for each key
-    # For example: key 'a' -> zip([1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7])
     aggregated_values = zip(*(d[key] for d in mm9N5to9))
-    
-    # Calculate the average for each position and store it in the result dictionary
+
     Average_amino[key] = [sum(values) / len(values) for values in aggregated_values]
 
 #%%
